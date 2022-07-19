504cc34c8993b8e907ed470def79fd37f748a6ac
Ivan Nguyen <ivannguyen@Ivan-Mac-4.local>
2022-07-19 09:09:29 +0700

    U: user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,14 +1,23 @@
-import { Prop, ModelOptions, getModelForClass } from '@typegoose/typegoose';
+import {
+  Prop,
+  ModelOptions,
+  getModelForClass,
+  Severity,
+} from '@typegoose/typegoose';
+import { Schema } from 'mongoose';
 
 export type UserDocument = User & Document;
 
-@ModelOptions({ schemaOptions: { _id: false } })
+@ModelOptions({
+  schemaOptions: { _id: true },
+  options: { allowMixed: Severity.ALLOW },
+})
 export class User {
   _id?: string;
   @Prop({ type: String })
   name?: string;
-  @Prop({ type: String })
-  avatar?: string;
+  @Prop({ type: () => Schema.Types.Mixed, default: {} })
+  avatar?: Record<string, any>;
   @Prop({ type: String })
   gender?: string;
   @Prop()
